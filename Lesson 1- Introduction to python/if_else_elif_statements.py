da744531448302b96fdcc6e47d86efdad425c2f6
sudarsanudash <sudarsanudash123@gmail.com>
2025-01-12 21:32:20 +1000

    Boolean, If, Else, Elif Statements

---
+++ Lesson 1- Introduction to python/if_else_elif_statements.py
@@ -0,0 +1,21 @@
+# name = input('Enter your name: ')
+# if name == 'Sudarsan':
+#     print('Hi Sudarsan')
+# else:
+#     print('Who are you?')
+
+#Vampires.py
+
+name = input('Enter your name: ')
+age = int(input('Enter your age:'))
+if name == 'Sudarsan':
+    print('Hi Sudarsan')
+
+elif age < 12:
+    print('You are not Sudarsan')
+
+elif age > 1000:
+    print('Are you a vampire? Cause Sudarsan is not')
+
+elif age > 100:
+    print('Sudarsan is not that old')
