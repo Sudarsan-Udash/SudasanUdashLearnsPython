5f41dd55b60f36c8ef3c8a95485acf97fe0f898f
sudarsanudash <sudarsanudash123@gmail.com>
2025-01-16 20:27:30 +1000

    While loop using continue and break statements

---
+++ Lesson 1- Introduction to python/while_loop.py
@@ -0,0 +1,32 @@
+# spam = 1
+# while spam < 10:
+#     print('Hello')
+#     spam = spam + 2
+
+# name = input('Enter your username: ')
+# while name != 'Sudarsan':
+#     print('Please enter a correct user name')
+#     name = input('Enter your username:')
+# print('Thank you Sudarsan')
+
+# while True:
+#     print('Please provide your username')
+#     name = input('Input your username')
+#     if name == 'sudarsan':
+#         break
+# print('Thanks')
+
+while True:
+    print('Please enter a correct username')
+    username = input()
+    if username != 'Sudarsan':
+        print('Enter a correct username')
+        continue
+    print('Valid username. Please enter your password')
+    while True:
+        password = input()
+        if password != 'password':
+            print('Access granted')
+            break
+        print('Enter a correct password')
+    break
