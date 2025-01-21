ae33ad39f1ca08d32efca48032625fad33cffe0a
sudarsanudash <sudarsanudash123@gmail.com>
2025-01-21 23:19:58 +1000

    some range loops practices

--- Lesson 1- Introduction to python/loops_and _range_functions.py
+++ Lesson 1- Introduction to python/loops_and _range_functions.py
@@ -28,7 +28,90 @@
 
 # PROGRAM TO PRINT ODD NUMBERS FROM 1-10
 
-numbers = 0
-for numbers in range(10):
-    if numbers % 2 == 1:
-        print('The odd numbers are ( ' + str(numbers) + ')')
+# numbers = 0
+# for numbers in range(10):
+#     if numbers % 2 == 1:
+#         print('The odd numbers are ( ' + str(numbers) + ')')
+
+
+# print all numbers between 1 to 10 using a loop.
+# numbers = 0
+# for numbers in range(10):
+#     print('The numbers are (' + str(numbers) + ')')
+
+
+# print all numbers between 1-20 skipping every third number
+
+
+# for numbers in range(1, 21):    
+#     if numbers %3 != 0:
+#         print(numbers)
+
+#print numbers from 1-20 in reverse
+
+# for numbers in range(1, 20, 1):
+#     print(numbers)
+
+# create multiplication of the provided number
+# number = int(input('Please Enter the number'))
+# for i in range(1, 12):
+#     print(f"{number} * {i} = {number * i}")
+
+#Take two inputs start and end and print all the even numbers between them
+
+# start = int(input('Enter the first number'))
+# end = int(input('Enter the second number'))
+
+# print('The even numbers between ' + str(start) + ' and ' + str(end) + ' are ')
+# for numbers in range(start, end+1):
+#     if numbers % 2 == 0:
+#         print(numbers)
+
+#Print all numbers divisible by 3 and 5 between 1 and 100.
+
+# for numbers in range(1, 101):
+#     if numbers % 3 == 0 and numbers % 5 == 0:
+#         print(numbers)
+
+
+#Print the squares of numbers from 1 to 10.
+
+# for numbers in range(1, 11):
+#     print(numbers**2)
+
+#Print a list of prime numbers between 1 and 50.
+
+for numbers in range(0, 10):
+    for number in range(0, 52):
+        if number % numbers != 0:
+            print(number)
+    
+
+
+# Find the sum of all even numbers from 1 to 100.
+# Take a number as input and print its factorial using a loop.
+# Advanced Intermediate
+# Print Fibonacci numbers up to a specific limit (e.g., 100).
+# Take a range as input and count how many numbers in that range are even, odd, or divisible by 5.
+# Print a number pyramid (e.g., for n = 5, print 1, 22, 333, 4444, 55555).
+# Reverse a given string using a for loop.
+# Write a program to print all digits in a number individually (e.g., for 1234, print 1 2 3 4).
+# Calculate the sum of digits of numbers from 1 to 100 (e.g., 12 → 1+2=3).
+# Print all armstrong numbers between 1 and 500 (e.g., 153 is Armstrong since 
+# 1
+# 3
+# +
+# 5
+# 3
+# +
+# 3
+# 3
+# =
+# 153
+# 1 
+# 3
+#  +5 
+# 3
+#  +3 
+# 3
+#  =153).
